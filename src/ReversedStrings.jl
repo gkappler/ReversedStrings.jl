9191778fb922f4b96764ec912aa9f0cd74e6c769
Gregor Kappler <gregor.kappler@bictxt.de>
2021-03-26 14:57:06 +0100

    Files generated by PkgTemplates

---
+++ src/ReversedStrings.jl
@@ -0,0 +1,5 @@
+module ReversedStrings
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ReversedStrings
+using Test
+
+@testset "ReversedStrings.jl" begin
+    # Write your own tests here.
+end
